fb222653cdeecc775ce753f0ecf0a4ce94c916c0
admin <admin@TeamCity>
2026-03-09 17:53:32 +0100

    runners of 'Build-n-Test' build configuration were updated (TeamCity change in 'EW-2026-Teamcity-Demo' project)

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -109,7 +109,7 @@ object BuildNTest : BuildType({
                 echo \#\#teamcity[testStarted name=\'LCD.PICTURE\' captureStandardOutput=\'true\']
                 rm -f test-image.jpg >/dev/null 2>/dev/null
                 v4l2-ctl --set-ctrl focus_automatic_continuous=0 --set-ctrl=focus_absolute=20
-                fswebcam test-image.jpg --skip 90 --rotate 180 --resolution 1920x1080 --crop 640x480 
+                fswebcam --skip 90 --rotate 180 --resolution 1920x1080 --crop 640x480 test-image.jpg  
                 
                 TIMESTAMP=${'$'}(date)
                 
